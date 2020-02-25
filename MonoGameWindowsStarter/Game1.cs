691404f103af2036ee0fad1f0143eef75ba59f1c
Skyler Kring <skring18@ksu.edu>
2020-02-25 13:45:55 -0600

    initial setup, need to create enemies and setup correct animations per corresponding state

--- MonoGameWindowsStarter/Game1.cs
+++ MonoGameWindowsStarter/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MonoGameWindowsStarter
 {
@@ -11,6 +13,10 @@ namespace MonoGameWindowsStarter
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        SpriteSheet sheet;
+        Player player;
+        List<Enemy> enemies;
+        AxisList world;
 
         public Game1()
         {
@@ -37,10 +43,24 @@ namespace MonoGameWindowsStarter
         /// </summary>
         protected override void LoadContent()
         {
+#if VISUAL_DEBUG
+            VisualDebugging.LoadContent(Content);
+#endif
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            var t = Content.Load<Texture2D>("spritesheet");
+            sheet = new SpriteSheet(t, 34, 34, 1, 2);
+
+            var playerFrames = from index in Enumerable.Range(19, 30) select sheet[index];
+            player = new Player(playerFrames);
+
+            world = new AxisList();
+            foreach (Enemy enemy in enemies)
+            {
+                world.AddGameObject(enemy);
+            }
         }
 
         /// <summary>
@@ -63,6 +83,10 @@ namespace MonoGameWindowsStarter
                 Exit();
 
             // TODO: Add your update logic here
+            player.Update(gameTime);
+
+            var enemyQuery = world.QueryRange(player.Bounds.X, player.Bounds.X + player.Bounds.Width);
+            player.CheckForEnemyCollision(enemyQuery);
 
             base.Update(gameTime);
         }
@@ -73,9 +97,31 @@ namespace MonoGameWindowsStarter
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
+
+            // Calculate and apply the world/view transform
+            var offset = new Vector2(200, 300) - player.Position;
+            var t = Matrix.CreateTranslation(offset.X, offset.Y, 0);
+            spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, t);
 
             // TODO: Add your drawing code here
+            var enemyQuery = world.QueryRange(player.Position.X - 221, player.Position.X + 400);
+            foreach (Enemy enemy in enemyQuery)
+            {
+                enemy.Draw(spriteBatch);
+            }
+            Debug.WriteLine($"{enemyQuery.Count()} Platforms rendered");
+
+            player.Draw(spriteBatch);
+
+            //draw certain amount of sprites in spritesheet on screen
+            for (var i = 0; i < 15; i++)
+            {
+                sheet[i].Draw(spriteBatch, new Vector2(i * 25, 25), Color.White);
+            }
+
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ MonoGameWindowsStarter/Player.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+
+
+namespace MonoGameWindowsStarter
+{
+    enum PlayerAnimationState
+    {
+        Idle,
+        MovingLeft,
+        MovingRight,
+        MovingUp,
+        MovingDown,
+        Dead
+    }
+
+    public class Player
+    {
+        const int FRAME_RATE = 300;
+
+        Sprite[] frames;
+        int currentFrame = 0;
+
+        PlayerAnimationState animationState = PlayerAnimationState.Idle;
+
+        int speed = 3;
+
+        TimeSpan animationTimer;
+        SpriteEffects spriteEffects = SpriteEffects.None;
+
+        Color color = Color.White;
+
+        Vector2 origin = new Vector2(15, 33);
+        public Vector2 Position = new Vector2(200, 200);
+        public BoundingRectangle Bounds => new BoundingRectangle(Position - 1.8f * origin, 34, 34);
+
+        /// <summary>
+        /// Constructs a new player
+        /// </summary>
+        /// <param name="frames">The sprite frames associated with the player</param>
+        public Player(IEnumerable<Sprite> frames)
+        {
+            this.frames = frames.ToArray();
+            animationState = PlayerAnimationState.MovingLeft;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            var keyboard = Keyboard.GetState();
+
+
+            // Horizontal movement
+            if (keyboard.IsKeyDown(Keys.Left))
+            {
+                animationState = PlayerAnimationState.MovingLeft;
+                Position.X -= speed;
+            }
+            else if (keyboard.IsKeyDown(Keys.Right))
+            {
+                animationState = PlayerAnimationState.MovingRight;
+                Position.X += speed;
+            }
+            else if (keyboard.IsKeyDown(Keys.Up))
+            {
+                animationState = PlayerAnimationState.MovingUp;
+                Position.Y -= speed;
+            }
+            else if (keyboard.IsKeyDown(Keys.Down))
+            {
+                animationState = PlayerAnimationState.MovingDown;
+                Position.Y += speed;
+            }
+            else
+            {
+                animationState = PlayerAnimationState.Idle;
+            }
+
+            // Apply animations
+            switch (animationState)
+            {
+                case PlayerAnimationState.Idle:
+                    currentFrame = 0;
+                    animationTimer = new TimeSpan(0);
+                    break;
+
+                case PlayerAnimationState.MovingLeft:
+                    animationTimer += gameTime.ElapsedGameTime;
+                    spriteEffects = SpriteEffects.None;
+                    // Walking frames are 9 & 10
+                    if (animationTimer.TotalMilliseconds > FRAME_RATE * 2)
+                    {
+                        animationTimer = new TimeSpan(0);
+                    }
+                    currentFrame = (int)Math.Floor(animationTimer.TotalMilliseconds / FRAME_RATE) + 9;
+                    break;
+
+                case PlayerAnimationState.MovingRight:
+                    animationTimer += gameTime.ElapsedGameTime;
+                    spriteEffects = SpriteEffects.FlipHorizontally;
+                    // Walking frames are 9 & 10
+                    if (animationTimer.TotalMilliseconds > FRAME_RATE * 2)
+                    {
+                        animationTimer = new TimeSpan(0);
+                    }
+                    currentFrame = (int)Math.Floor(animationTimer.TotalMilliseconds / FRAME_RATE) + 15;
+                    break;
+
+            }
+        }
+
+        public void CheckForEnemyCollision(IEnumerable<IBoundable> enemies)
+        {
+            foreach (Enemy enemy in enemies)
+            {
+                if (Bounds.CollidesWith(enemy.Bounds))
+                {
+                    animationState = PlayerAnimationState.Dead;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Render the player sprite.  Should be invoked between 
+        /// SpriteBatch.Begin() and SpriteBatch.End()
+        /// </summary>
+        /// <param name="spriteBatch">The SpriteBatch to use</param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+#if VISUAL_DEBUG 
+            VisualDebugging.DrawRectangle(spriteBatch, Bounds, Color.Red);
+#endif
+            frames[currentFrame].Draw(spriteBatch, Position, color, 0, origin, 2, spriteEffects, 1);
+        }
+
+    }
+}
